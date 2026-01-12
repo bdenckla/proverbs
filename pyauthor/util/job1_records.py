fbdc581148555e95a1f3b28f4b618aed9e50ec3e
Ben Denckla <bdenckla@alum.mit.edu>
2026-01-12 17:08:50 -0500

    list other things turned up in BHQ Apparatus

--- pyauthor/util/job1_records.py
+++ pyauthor/util/job1_records.py
@@ -6,23 +6,25 @@
 # https://manuscripts.sefaria.org/leningrad-color/BIB_LENCDX_F402A.jpg
 # column 1 line 7
 #
-# 02:08   לְהִתְגָּרֵ֖ר ML (err) | לְהִתְגָּרֵ֖ד MA MY (resh/dalet)
-# 38:17 צַלְמָּ֣וֶת ML (err) | צַלְמָ֣וֶת MA MY (dagesh)
-# 16:13 יַחְמ֑וֹל ML (err) | יַחְמֹ֑ל MA MY (xaser/malei)
-# 17:11 לְבָבִּֽי׃ ML (err) | לְבָבִֽי׃ MA MY (dagesh)
-# 18:09 בְּעָקֵ֣ב ML (err) | בְּעָקֵ֣ב MA MY (?)
-# 22:30 וְ֝נִּמְלַ֗ט ML (err) | וְ֝נִמְלַ֗ט MA MY (dagesh)
-# 24:16 יָ֥דְּעוּ ML (err) | יָ֥דְעוּ MA MY (dagesh)
-# כִּבְלִי־ ML (err) | בִּבְלִ־ MA MY(qere) (bet/kaf)
-# 42:13 בָנֽוֹת׃ ML (err) | בָּנֽוֹת׃ MA MY (dagesh)
 # 01:21 יָצָ֜תִי MA MY | יצתי ML(ket) | יָצָ֜אתִי ML(qere) (differ-orth) 
 # 02:07 ְעַ֥ד MA MY | עד Mket | וְעַד Mqere 
+# 02:08 לְהִתְגָּרֵ֖ר ML (err) | לְהִתְגָּרֵ֖ד MA MY (resh/dalet)
 # 05:12 תּוּשִׁיָּֽה׃ ML (differ-orth) | תֻּשִׁיָּֽה׃ MA MY 
 # 06:21 לֹ֑א MA MY (T) | לא ML(ket) | ל֑וֹ ML(qere) (err-lex) 
-# 10:22
-# 14:05
-# 15:31
-# 18:15
+# 10:22 עֵיפָ֨תָה ML (differ-orth) | עֵפָ֨תָה MA MY
+# 14:05 חֻקָּ֥ו MA | חקו ML(ket) MY(ket) | חֻקָּיו ML(qere) MY(qere) (differ-orth)
+# 15:31 בַּשָּׁ֣ו MA MY | בשו ML(ket) | בַּשָּׁ֣יו ML(qere) (differ-orth)
+# 16:13 יַחְמ֑וֹל ML (err) | יַחְמֹ֑ל MA MY (xaser/malei)
+# 17:06 וְתֹ֖פֶתּ ML (err) | וְתֹ֖פֶת MA MY ○ וְתֹ֖פֶתּ (dagesh) (see above)
+# 17:11 לְבָבִּֽי׃ ML (err) | לְבָבִֽי׃ MA MY (dagesh)
+# 18:09 בְּעָקֵ֣ב ML (err) | בְּעָקֵ֣ב MA MY (?)
+# 18:15 מִבְּלִיל֑וֹ ML MY | מִבְּלִי־ל֑וֹ MA (differ-orth)
+# 22:30 וְ֝נִּמְלַ֗ט ML (err) | וְ֝נִמְלַ֗ט MA MY (dagesh)
+# 24:16 יָ֥דְּעוּ ML (err) | יָ֥דְעוּ MA MY (dagesh)
+# 26:03 וְ֝תוּשִׁיָּ֗ה ML (differ-orth) | וְ֝תֻשִׁיָּ֗ה MA MY(qere)
+# 36:12 כִּבְלִי־ ML (err) | בִּבְלִ־ MA MY(qere) (bet/kaf)
+# 38:17 צַלְמָּ֣וֶת ML (err) | צַלְמָ֣וֶת MA MY (dagesh)
+# 42:13 בָנֽוֹת׃ ML (err) | בָּנֽוֹת׃ MA MY (dagesh)
 
 
 _QUOTE_FORCED = "“forced”"
